ed8765a6d76f6dcb0fad2e179461af152eae358b
Ahmed Hafez <Ahmed41901@gmail.com>
2019-04-17 23:34:12 +0200

    Initial project distribution

---
+++ SalesManagment/Models/Database/DataConnection.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+namespace SalesManagment
+{
+    public static class DataConnection
+    {
+        private static SqlConnection Connection;
+        private static string connectionString;
+
+        static DataConnection()
+        {
+            connectionString = @"Data Source=.\SQLEXPRESS01;Initial Catalog=ProductDB;Integrated Security=True;";
+            Connection = new SqlConnection(connectionString);
+        }
+
+        private static void Open() => Connection.Open();
+
+        private static void Close() => Connection.Close();
+        
+        public static DataTable SelectData(string stored_procedure, SqlParameter[] param)
+        {
+            Open();
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.CommandText = stored_procedure;
+            sqlCmd.Connection = Connection;
+
+            if (param != null)
+                sqlCmd.Parameters.AddRange(param);
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCmd);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            Close();
+
+            return dataTable;
+        }
+
+        public static void ExcuteCommand(string stored_procedure, params SqlParameter[] param)
+        {
+            Open();
+
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.CommandText = stored_procedure;
+
+            if (param != null)
+                sqlCmd.Parameters.AddRange(param);
+
+            sqlCmd.ExecuteNonQuery();
+
+            Close();
+        }
+    }
+}
---
+++ SalesManagment/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SalesManagment
+{
+    class RelayCommand : ICommand
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The action to run
+        /// </summary>
+        private Action mAction;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand(Action action)
+        {
+            mAction = action;
+        }
+
+        #endregion
+
+        #region Public Members
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the commands Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            mAction();
+        }
+
+        #endregion
+    }
+}
