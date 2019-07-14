0747d57eea3de15ae81c023716e4b725a26e8167
Ahmed Hafez <Ahmed41901@gmail.com>
2019-07-14 13:18:26 +0200

    Create interfaces for the Factory Design Pattern

---
+++ SalesManagment/DataModels/IFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesManagment
+{
+    /// <summary>
+    /// Factory Design Pattern
+    /// </summary>
+    public interface IFactory
+    {
+        /// <summary>
+        /// The factory method to create instances
+        /// </summary>
+        object GetInstances(object obj);
+    }
+}
