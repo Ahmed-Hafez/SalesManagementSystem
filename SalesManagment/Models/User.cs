cc1f66d7260b1daf0779801ac25525fda5d59e49
Ahmed Hafez <Ahmed41901@gmail.com>
2019-07-14 13:19:29 +0200

    Adding some functionality
    Adding Login functionality

--- SalesManagment/Models/User.cs
+++ SalesManagment/Models/User.cs
@@ -1,7 +1,15 @@
-﻿namespace SalesManagment
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SalesManagment
 {
+    /// <summary>
+    /// The logic of the user of the application
+    /// </summary>
     public class User
     {
+        #region Public Properties
+
         /// <summary>
         /// The username of the user
         /// </summary>
@@ -15,6 +23,49 @@
         /// <summary>
         /// The type of the user
         /// </summary>
-        public string UserType { get; set; }
+        public UserType UserType { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Login the user
+        /// </summary>
+        /// <param name="username">username of the user</param>
+        /// <param name="password">password of the user</param>
+        /// <param name="userType">userType of the user</param>
+        /// <returns></returns>
+        public bool Login(string username, string password, UserType userType)
+        {
+            SqlParameter[] sqlParameters = new SqlParameter[3];
+            sqlParameters[0] = new SqlParameter("@ID", SqlDbType.VarChar);
+            sqlParameters[0].Value = username;
+            sqlParameters[1] = new SqlParameter("@Password", SqlDbType.VarChar);
+            sqlParameters[1].Value = password;
+            sqlParameters[2] = new SqlParameter("@UserType", SqlDbType.VarChar);
+            sqlParameters[2].Value = userType.ToString();
+
+            if (DataConnection.SelectData("Login_Procedure", sqlParameters).Rows.Count > 0)
+            {
+                Username = username;
+                Password = password;
+                UserType = userType;
+
+                return true;
+            }
+            return false;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// The user type of the application
+    /// </summary>
+    public enum UserType
+    {
+        Manager,
+        Employee
     }
 }
