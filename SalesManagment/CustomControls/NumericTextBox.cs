e68e81e780c10c191a999b6e38c721640540f91d
Ahmed Hafez <Ahmed41901@gmail.com>
2019-07-16 04:37:59 +0200

    Create NumberTextBox control to accept only numeric data

---
+++ SalesManagment/CustomControls/NumericTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SalesManagment
+{
+    /// <summary>
+    /// Text box accepts only numeric data
+    /// </summary>
+    public class NumericTextBox : TextBox
+    {
+        public NumericTextBox()
+        {
+            this.PreviewTextInput += NumericTextBox_PreviewTextInput;
+        }
+
+        private void NumericTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
