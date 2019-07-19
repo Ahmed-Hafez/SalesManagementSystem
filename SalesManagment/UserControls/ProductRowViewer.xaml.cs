75d8322d894b6a918a198c25cb6bd719d1663e49
Ahmed Hafez <Ahmed41901@gmail.com>
2019-07-19 06:26:33 +0200

    Adding the ProductDecription part

--- SalesManagment/UserControls/ProductRowViewer.xaml.cs
+++ SalesManagment/UserControls/ProductRowViewer.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,35 @@ namespace SalesManagment
     /// </summary>
     public partial class ProductRowViewer : UserControl
     {
+        private bool DescriptionGridOpened;
+        private DoubleAnimation openDescriptionGrid;
+        private DoubleAnimation closeDescriptionGrid;
+        private const double openingHeight = 126;
+
         public ProductRowViewer()
         {
             InitializeComponent();
+
+            DescriptionGrid.Height = 0;
+            DescriptionGridOpened = false;
+
+            openDescriptionGrid = new DoubleAnimation(0, openingHeight, TimeSpan.FromMilliseconds(300));
+            closeDescriptionGrid = new DoubleAnimation(openingHeight, 0, TimeSpan.FromMilliseconds(300));
         }
+
+        private void notesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!DescriptionGridOpened)
+            {
+                DescriptionGridOpened = true;
+                DescriptionGrid.BeginAnimation(HeightProperty, openDescriptionGrid);
+            }
+            else
+            {
+                DescriptionGridOpened = false;
+                DescriptionGrid.BeginAnimation(HeightProperty, closeDescriptionGrid);
+            }
+        }
+
     }
 }
