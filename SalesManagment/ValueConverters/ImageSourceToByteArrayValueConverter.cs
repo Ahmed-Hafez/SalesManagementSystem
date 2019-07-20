c5bc7c71af60269590a04d8f018a06c11bd480d3
Ahmed Hafez <Ahmed41901@gmail.com>
2019-07-20 16:44:07 +0200

    Update ImageSourceToByteArrayValueConverter

--- SalesManagment/ValueConverters/ImageSourceToByteArrayValueConverter.cs
+++ SalesManagment/ValueConverters/ImageSourceToByteArrayValueConverter.cs
@@ -6,46 +6,32 @@ using System.Windows.Media.Imaging;
 
 namespace SalesManagment
 {
+    /// <summary>
+    /// A converter that takes a string which is a path for image on the PC and converts it to byte array
+    /// </summary>
     public class ImageSourceToByteArrayValueConverter : BaseValueConverter<ImageSourceToByteArrayValueConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // parameter value should be string (The path of the image on the pc)
-
             // TODO : Exception Handling for unexpected errors here
-            FileStream fileStream = new FileStream((string)value, FileMode.Open);
-            BinaryReader reader = new BinaryReader(fileStream);
 
-            byte[] binaryData = reader.ReadBytes(System.Convert.ToInt32(reader.BaseStream.Length - 1));
-            reader.Close();
+            if (value is string)
+            {
+                FileStream fileStream = new FileStream((string)value, FileMode.Open);
+                BinaryReader reader = new BinaryReader(fileStream);
 
-            return binaryData;
+                byte[] binaryData = reader.ReadBytes(System.Convert.ToInt32(reader.BaseStream.Length - 1));
+                reader.Close();
+
+                return binaryData;
+            }
+
+            return null;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // parameter value should be byte[]
-
-            if (!(value is byte[]))
-                return null;
-
-            byte[] binaryData = (byte[])value;
-
-            MemoryStream strm = new MemoryStream();
-            strm.Write(binaryData, 0, binaryData.Length);
-            strm.Position = 0;
-            System.Drawing.Image img = System.Drawing.Image.FromStream(strm);
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            ImageSource source = bitmapImage;
-
-            MemoryStream ms = new MemoryStream();
-            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-            ms.Seek(0, SeekOrigin.Begin);
-            bitmapImage.StreamSource = ms;
-            bitmapImage.EndInit();
-
-            return source;
+            throw new NotImplementedException();
         }
     }
 }
