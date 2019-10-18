75bcf4673915f616f93ff7dd3e94397436586c79
Ahmed Hafez <Ahmed41901@gmail.com>
2019-10-18 08:58:09 +0200

    Adding base class for row viewers

---
+++ SalesManagment/UserControls/Base/BaseRowViewer.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SalesManagment
+{
+    public class BaseRowViewer : UserControl
+    {
+        /// <summary>
+        /// Inititalizes a new instance from <see cref="BaseRowViewer"/> class
+        /// </summary>
+        public BaseRowViewer()
+        {
+            this.Loaded += ProductRowViewer_Loaded;
+        }
+
+        private async void ProductRowViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            await this.SlideAndFadeInFromLeft(400, keepMargin: false);
+        }
+    }
+}
