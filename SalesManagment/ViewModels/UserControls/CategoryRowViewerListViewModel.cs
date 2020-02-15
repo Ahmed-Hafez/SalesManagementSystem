3f5fd60177a0025eb8e28c6842934b528825896f
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2020-02-15 18:54:22 +0200

    Adding the view models to CategoryRowViewerList and CategoryRowViewer

---
+++ SalesManagment/ViewModels/UserControls/CategoryRowViewerListViewModel.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace SalesManagment
+{
+    public class CategoryRowViewerListViewModel : BaseRowViewerListViewModel
+    {
+        #region Private Members
+
+        /// <summary>
+        /// The type of search on the categories list
+        /// </summary>
+        private CategorySearchType mCategorySearchType = CategorySearchType.Name;
+
+        #endregion
+
+        #region Public Properties 
+
+        /// <summary>
+        /// The type of search on the categories list
+        /// </summary>
+        public CategorySearchType CategorySearchType
+        {
+            get { return mCategorySearchType; }
+            set
+            {
+                mCategorySearchType = value;
+                if (SearchText == "")
+                    SearchTag = "Write the category " + mCategorySearchType.ToString();
+                else
+                    searchHandler.Invoke();
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance from the <see cref="CategoryRowViewerListViewModel"/> class
+        /// </summary>
+        public CategoryRowViewerListViewModel()
+        {
+            this.CategorySearchType = CategorySearchType.ID;
+
+            var list = Category.GetAllCategories()?.ConvertAll(
+                    (category) => ConvertToCategoryRowViewer(category));
+
+            if (list != null)
+                AllItems = new ObservableCollection<BaseRowViewerViewModel>(list);
+            else
+                AllItems = new ObservableCollection<BaseRowViewerViewModel>();
+
+            Items = new ObservableCollection<BaseRowViewerViewModel>();
+
+            LoadAnimationDuration = 50;
+
+            Fill_List();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Searches in the categories list
+        /// </summary>
+        /// <param name="type">The search type</param>
+        /// <param name="searchText">The text to search about it</param>
+        protected override ObservableCollection<BaseRowViewerViewModel> Search()
+        {
+            var foundedItems = new ObservableCollection<BaseRowViewerViewModel>();
+            for (int i = 0; i < AllItems.Count; i++)
+            {
+                switch (CategorySearchType)
+                {
+                    case CategorySearchType.ID:
+                        if (((CategoryRowViewerViewModel)AllItems[i]).ID.ToString().ToLower().StartsWith(SearchText.ToLower()))
+                            foundedItems.Add(AllItems[i]);
+                        break;
+                    case CategorySearchType.Name:
+                        if (((CategoryRowViewerViewModel)AllItems[i]).Name.ToString().ToLower().StartsWith(SearchText.ToLower()))
+                            foundedItems.Add(AllItems[i]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return foundedItems;
+        }
+
+        /// <summary>
+        /// Matches the passed view model with the text in the searchBox depending on
+        /// the type of search
+        /// </summary>
+        /// <param name="viewModel">The view model to match with</param>
+        protected override bool IsMatch(BaseRowViewerViewModel viewModel)
+        {
+            switch (CategorySearchType)
+            {
+                case CategorySearchType.ID:
+                    if (((CategoryRowViewerViewModel)viewModel).ID.ToString().ToLower().StartsWith(SearchText))
+                        return true;
+                    break;
+                case CategorySearchType.Name:
+                    if (((CategoryRowViewerViewModel)viewModel).Name.ToString().ToLower().StartsWith(SearchText))
+                        return true;
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Deletes the specified item from the list
+        /// </summary>
+        /// <param name="viewModel">The view model of the item to delete</param>
+        protected override void DeleteItem(BaseRowViewerViewModel viewModel)
+        {
+            // Delete from the items list
+            Items.Remove(viewModel);
+
+            // Delete from the database
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@Category_ID", SqlDbType.Int);
+            sqlParameters[0].Value = ((CategoryRowViewerViewModel)viewModel).ID;
+
+            DataConnection.ExcuteCommand("Delete_Category_Procedure", sqlParameters);
+        }
+
+        protected override void EditItem(BaseRowViewerViewModel viewModel)
+        {
+            var categoryRowViewerViewModel = viewModel as CategoryRowViewerViewModel;
+
+            if (categoryRowViewerViewModel.IsEditable)
+            {
+                categoryRowViewerViewModel.Name = categoryRowViewerViewModel.EditedName;
+                Category category = new Category(categoryRowViewerViewModel.ID, categoryRowViewerViewModel.Name);
+                categoryRowViewerViewModel.IsEditable = false;
+                category.Edit();
+            }
+            else
+            {
+                categoryRowViewerViewModel.IsEditable = true;
+            }
+        }
+
+        protected override void PrintItem(BaseRowViewerViewModel viewModel)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected void CancelEditing(BaseRowViewerViewModel viewModel)
+        {
+            var categoryRowViewerViewModel = viewModel as CategoryRowViewerViewModel;
+
+            categoryRowViewerViewModel.IsEditable = false;
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Converting the <see cref="Category"/> object 
+        /// to <see cref="CategoryRowViewer"/> object using <see cref="CategoryRowViewerViewModel"/>
+        /// </summary>
+        /// <param name="category">The category to convert</param>
+        private CategoryRowViewerViewModel ConvertToCategoryRowViewer(Category category)
+        {
+            var viewModel = new CategoryRowViewerViewModel
+            {
+                ID = category.ID,
+                Name = category.Name
+            };
+
+            viewModel.Deleted += DeleteItem;
+            viewModel.Edited += EditItem;
+            viewModel.Cancel += CancelEditing;
+
+            return viewModel;
+        }
+
+        #endregion
+
+    }
+}
---
+++ SalesManagment/ViewModels/UserControls/CategoryRowViewerViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SalesManagment
+{
+    public class CategoryRowViewerViewModel : BaseRowViewerViewModel
+    {
+        #region Public Events
+
+        /// <summary>
+        /// Fires when editing action is canceled
+        /// </summary>
+        public event Action<BaseRowViewerViewModel> Cancel;
+
+        #endregion
+
+        #region Private Members
+
+        /// <summary>
+        /// Determine whether the row viewer data can be edited or not
+        /// </summary>
+        private bool mIsEditable = false;
+
+        #endregion
+
+        #region Public Properties
+
+        #region Category Data
+
+        /// <summary>
+        /// The ID of the category
+        /// </summary>
+        public long ID { get; set; }
+
+        /// <summary>
+        /// The Name of the category
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Determine whether the row viewer data can be edited or not
+        /// </summary>
+        public bool IsEditable
+        {
+            get => mIsEditable;
+            set
+            {
+                mIsEditable = value;
+                if (mIsEditable == true)
+                    EditedName = Name;
+            }
+        }
+
+        /// <summary>
+        /// The Text printed in Name Textbox
+        /// </summary>
+        public string EditedName { get; set; }
+
+        #region Public Commands
+
+        /// <summary>
+        /// The command used to cancel editing operation
+        /// </summary>
+        public ICommand CancelCommand { get; set; }
+
+        public override ICommand DeleteCommand { get; set; }
+        public override ICommand EditCommand { get; set; }
+        public override ICommand PrintCommand { get; set; }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initialize new instance from <see cref="CategoryRowViewerViewModel"/> class
+        /// </summary>
+        public CategoryRowViewerViewModel()
+        {
+            CancelCommand = new RelayCommand(() => CancelEditing());
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Notifing the list which containing this item that editing action was canceled
+        /// </summary>
+        private void CancelEditing()
+        {
+            Cancel?.Invoke(this);
+        }
+
+        #endregion
+    }
+}
