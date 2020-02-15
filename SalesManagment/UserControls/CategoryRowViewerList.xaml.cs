f1601ce946f76ded585d0bc7200f7f7b23fbd286
Ahmed Hafez <41956720+Ahmed-Hafez@users.noreply.github.com>
2020-02-15 18:53:47 +0200

    Creating CategoryRowViewerList

---
+++ SalesManagment/UserControls/CategoryRowViewerList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalesManagment
+{
+    /// <summary>
+    /// Interaction logic for CategoryRowViewerList.xaml
+    /// </summary>
+    public partial class CategoryRowViewerList : UserControl
+    {
+        public CategoryRowViewerList()
+        {
+            InitializeComponent();
+        }
+    }
+}
